97f3271386bfa965eb2eed61eff0fd178c96d296
vaishnavi <vaishnavishukla01@gmail.com>
2024-06-21 12:40:40 +0530

    social-media10

---
+++ app.js
@@ -0,0 +1,61 @@
+require("dotenv").config({ path: "./.env" });
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+
+// db connection
+const db = require("./models/database");
+db.connect();
+var userRoutes = require("./routes/user.routes");
+var indexRoutes = require("./routes/index.routes");
+
+var app = express();
+
+const session = require("express-session");
+const passport = require("passport");
+const UserCollection = require("./models/user.schema");
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: true,
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+passport.serializeUser(UserCollection.serializeUser());
+passport.deserializeUser(UserCollection.deserializeUser());
+
+app.use("/", indexRoutes);
+app.use("/user", userRoutes);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render("error");
+});
+
+module.exports = app;
